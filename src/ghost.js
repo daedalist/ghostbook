86cd7e20ad552c9a87ab0cd559e32bfc427b08a8
David Byrne <drbyrne@daedalist.com>
2020-10-24 18:14:22 +1100

    Render one ghost object

---
+++ src/ghost.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const evidence = {
+    GHOST_ORB: 'Ghost orb',
+    SPIRIT_BOX: 'Spirit box',
+    FINGERPRINTS: 'Fingerprints',
+    EMF_5: 'EMF Level 5',
+    FREEZING: 'Freezing temperatures',
+    GHOST_WRITING: 'Ghost writing',
+}
+
+class Ghost extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const evidences = this.props.evidence_list.map(
+            (e) => {
+                return (<li>{e}</li>);
+            }
+        );
+        return (
+            <div className="ghost">
+                <div className="name">{this.props.name}</div>
+                <ul className="evidenceList">
+                    {evidences}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Ghost;
