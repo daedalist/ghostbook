b140e2e1d4338e813a7206af1479ffb45fe8e821
David Byrne <drbyrne@daedalist.com>
2021-08-28 10:39:40 +1000

    Exposition update.
    Add new ghosts Myling, Goryo
    Add new evidence DOTS projector
    Match evidence to new update

--- src/ghost.js
+++ src/ghost.js
@@ -7,6 +7,7 @@ export const evidence = {
     EMF_5: 'EMF Level 5',
     FREEZING: 'Freezing temperatures',
     GHOST_WRITING: 'Ghost writing',
+    DOTS_PROJECTOR: 'D.O.T.S projector',
 }
 
 class Ghost extends React.Component {
