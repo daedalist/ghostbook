a266c5574553f1cf6e435b4436e5e6e2d8f59ad5
David Byrne <drbyrne@daedalist.com>
2022-01-27 12:40:35 +1100

    Render behavioural strengths & weaknesses in a toggleable "details" section.

--- src/ghost.js
+++ src/ghost.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ghost_data_map from './ghost_data_map.json';
+
 export const evidence = {
     GHOST_ORB: 'Ghost orb',
     SPIRIT_BOX: 'Spirit box',
@@ -22,9 +24,18 @@ class Ghost extends React.Component {
                 return (<li className="fakeEvidence" key={e}>{e}</li>);
             }
         )
+        const strength = ghost_data_map[0][this.props.name]["strength"];
+        const weakness = ghost_data_map[0][this.props.name]["weakness"];
+
         return (
             <div className="ghost">
-                <div className="name">{this.props.name}</div>
+                <div className="ghostName">{this.props.name}</div>
+                <input type="checkbox" id="showDetails" />
+                <label for="showDetails">details</label>
+                <div className="ghostDetail">
+                    <p>{strength}</p>
+                    <p>{weakness}</p>
+                </div>
                 <ul className="evidenceList">
                     {evidences}
                     {fake_evidences}
